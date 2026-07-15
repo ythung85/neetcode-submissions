217101031c1a7d9513abb68ed2f81d9d9b28eb76
Noah Hung <55910900+ythung85@users.noreply.github.com>
2026-07-14 17:40:30 -0700

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        
+
+        int l = 1;
+        int r = *max_element(piles.begin(), piles.end());
+        int res = r;
+        
+        while(l <= r){
+            int m = l + (r-l)/2;
+            long long total = 0;
+
+            for(int n: piles){
+                total += (n+m-1)/ m;
+            }
+            if(total <= h){
+                res = m;
+                r = m - 1;
+            }else{
+                l = m + 1;
+            }
+
+        }
+
+        return res;
+    }
+};
